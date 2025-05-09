7c09cf7a7c2053b2064745722ca4fc550d97ac5c
usunwshineu <162890968+usunwshineu@users.noreply.github.com>
2025-05-09 16:13:26 +0300

    update

--- arm/arm.ino
+++ arm/arm.ino
@@ -4,6 +4,7 @@
 const uint8_t trigPin = 30;
 const uint8_t echoPin = 32;
 
+long duration;
 int distance;
 
 Servo base;
@@ -33,12 +34,11 @@ int getDistanceCM() {
 
 int scanForBottle() {
   for (int baseAngle = 0; baseAngle <= 150; baseAngle += 2) {
-    Braccio.ServoMovement(20, baseAngle, 140, 165, 50, 90, 10);
+    Braccio.ServoMovement(20, baseAngle, 140, 110, 85, 90, 10);
     delay(300);
     int distance = getDistanceCM();
     if (distance < 6 && !(baseAngle > 95 && baseAngle < 105)) {
-      Serial.print(baseAngle);
-      return baseAngle;
+      return baseAngle+2;
     }
   }
   return -1; // Not found
@@ -49,25 +49,26 @@ void loop() {
   int detectedAngle = scanForBottle();
   if (detectedAngle != -1) {
 
-    Braccio.ServoMovement(20, detectedAngle, 140, 165, 50, 90, 10); // starting pos
+    Braccio.ServoMovement(20, detectedAngle, 140, 110, 85, 90, 10); // starting pos
     delay(1000);
-    Braccio.ServoMovement(20, detectedAngle, 140, 175, 50, 90, 10); // move to bottle
-    Braccio.ServoMovement(10, detectedAngle, 140, 175, 50, 90, 73); // close grip
-    Braccio.ServoMovement(20, detectedAngle, 140, 145, 30, 90, 73); // lift
+    Braccio.ServoMovement(20, detectedAngle, 160, 110, 90, 90, 10); // move to bottle
+    Braccio.ServoMovement(20, detectedAngle, 160, 110, 90, 90, 73); // close grip
+    Braccio.ServoMovement(20, detectedAngle, 100, 120, 90, 90, 73); // lift
+    Braccio.ServoMovement(10, detectedAngle, 100, 130, 90, 90, 73);
 
     // Move to plant
-    Braccio.ServoMovement(20, 150, 140, 145, 30, 90, 73); // plant position
-    Braccio.ServoMovement(20, 150, 140, 145, 30, 15, 73); // pour
+    Braccio.ServoMovement(20, 150, 100, 130, 90, 90, 73); // plant position
+    Braccio.ServoMovement(20, 150, 100, 130, 90, 0, 73); // pour
     delay(1000); // simulate watering
 
     // Return bottle
-    Braccio.ServoMovement(20, 150, 140, 145, 30, 90, 73); // straighten
-    Braccio.ServoMovement(20, detectedAngle, 140, 145, 30, 90, 73); // return
-    Braccio.ServoMovement(10, detectedAngle, 140, 175, 50, 90, 73); //get down
-    Braccio.ServoMovement(20, detectedAngle,  140, 175, 50, 90, 10); // open gripper
+    Braccio.ServoMovement(20, 150, 100, 130, 90, 90, 73); // straighten
+    Braccio.ServoMovement(20, detectedAngle, 100, 130, 90, 90, 73); // return
+    Braccio.ServoMovement(10, detectedAngle, 150, 120, 90, 90, 73); //get down
+    Braccio.ServoMovement(20, detectedAngle,  150, 120, 90, 90, 10); // open gripper
 
     // Idle pose
-    Braccio.ServoMovement(20, 90, 90, 90, 90, 90, 10); // idle
+    Braccio.ServoMovement(20, detectedAngle, 90, 90, 90, 90, 10); // idle
     delay(5000);
   }
 }
