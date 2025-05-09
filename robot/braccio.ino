43752c4408cf2746b23618236d4052540c431a8f
IttuAlex <ittu.alexandru93@gmail.com>
2025-05-09 12:33:14 +0300

    robot functie ridicare

--- robot/braccio.ino
+++ robot/braccio.ino
@@ -1,7 +1,6 @@
 #include <Braccio.h>
 #include <Servo.h>
 
-// Servo-urile individuale
 Servo base;
 Servo shoulder;
 Servo elbow;
@@ -9,35 +8,48 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
-void ridicare()
-
 void setup() {
-   
-   Braccio.begin();
-
+  Braccio.begin();
   delay(200);
-
-    // merge in laterala stanga aproape pe pozitia cubului
-   Braccio.ServoMovement(20,    120,75,80,40,90,10);
-    delay(400);
-    // merge pe pozitia cubului
-    Braccio.ServoMovement(20,    138,75,75,40,90,10);
-
-  delay(500);
-  // se apleaca pe pozitia cubului
-  Braccio.ServoMovement(20,    138,65,65,40,90,10);
-  delay(800);
-
-  // prinde cubul
-  Braccio.ServoMovement(20,    134,55,65,40,90,70);
-
-  delay(500);
-
-  //ridica cubul
-  Braccio.ServoMovement(20,    134,55,90,50,90,70);
-
+  ridicare(2);
 }
 
 void loop() {
   // nimic
+}
+
+void ridicare(int nr){
+
+  for(int i=0; i<nr; i++){
+    Braccio.ServoMovement(20, 120, 75, 80, 40, 90, 10);
+    delay(400);
+
+    // merge pe pozitia cubului
+    Braccio.ServoMovement(20, 138, 75, 75, 40, 90, 10);
+    delay(500);
+
+    // se apleaca
+    Braccio.ServoMovement(20, 138, 65, 65, 40, 90, 10);
+    delay(800);
+
+    // prinde
+    Braccio.ServoMovement(20, 134, 55, 65, 40, 90, 70);
+    delay(500);
+
+    // ridica
+    Braccio.ServoMovement(20, 134, 55, 90, 50, 90, 70);
+    delay(400);
+
+    // duce spre stânga
+    Braccio.ServoMovement(20, 174, 55, 90, 50, 90, 70);
+    delay(400);
+
+    // poziție de lăsare
+    Braccio.ServoMovement(20, 174, 55, 65, 50, 90, 70);
+    delay(400);
+
+    // deschide gripper
+    Braccio.ServoMovement(20, 174, 55, 65, 50, 90, 10);
+    delay(500);
+  }
 }
