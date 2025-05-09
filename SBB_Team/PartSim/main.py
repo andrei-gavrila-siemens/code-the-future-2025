8d9a928b434843aed620220c45995be5424ac750
Andrei <ascurtuny@gmail.com>
2025-05-09 12:03:56 +0300

    Better way of drawing particles

--- SBB_Team/PartSim/main.py
+++ SBB_Team/PartSim/main.py
@@ -1,51 +1,44 @@
 import pygame
 import random
-import math
+
+
+
 
 pygame.init()
-
 W, H = 1000, 500
 screen = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Particle Display")  
 clock = pygame.time.Clock()
 
-colors = {
-    "red": (255, 80, 80),
-    "green": (80, 255, 80),
-    "blue": (80, 80, 255)
-}
+
+
+PARTICLE_COUNT = 200
+PREDEFINED_COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)] 
 PARTICLE_RADIUS = 3
 
-particles = []
 
-class Particle:
-    def __init__(self, x, y, color_name):
-        self.x = x
-        self.y = y
-        self.color_name = color_name
-        
-    def draw(self):
-        pygame.draw.circle(screen, colors[self.color_name], (int(self.x), int(self.y)), PARTICLE_RADIUS) # last number is the radius
-                          
-def add_particles(color_name, count):
-    for _ in range(count):
-        x = random.uniform(0, W)
-        y = random.uniform(0, H)
-        particles.append(Particle(x, y, color_name))
-        
-add_particles("red", 5)
-add_particles("blue", 5)
-        
+particles = []
+for _ in range(PARTICLE_COUNT):
+    x = random.randint(PARTICLE_RADIUS, W - PARTICLE_RADIUS) 
+    y = random.randint(PARTICLE_RADIUS, H - PARTICLE_RADIUS)
+    color = random.choice(PREDEFINED_COLORS)
+    particles.append([x, y, color])
+
+
+
 running = True
 while running:
-    screen.fill((0, 0, 0))
-    for p in particles:
-        p.draw()
-    
-    pygame.display.flip()
-    clock.tick(60)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+    
+    screen.fill((0, 0, 0))
+    
+    
+    for x, y, color in particles:
+        pygame.draw.circle(screen, color, (x, y), PARTICLE_RADIUS)
+    
+    pygame.display.flip()
+    clock.tick(60)
+
 pygame.quit()
