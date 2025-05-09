3b9cdc147625bbaa45849611f6d3f21d73355cb7
Bucuroiu Emil <150169607+EmilB30@users.noreply.github.com>
2025-05-09 18:51:59 +0300

    fix: better control of Robotic Arm
    Improved control of arm with smoother and more motors activated by button, can use the claw as well.

---
+++ bratroboticV2.ino
@@ -0,0 +1,78 @@
+#include <Braccio.h>
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
+const int joyX = A0;
+const int joyY = A1;
+const int buttonA = 2;
+const int buttonB = 4;
+const int buttonE = 7; 
+
+const int threshold = 100;
+
+int baseAngle = 0;
+int shoulderAngle = 90;
+int elbowAngle = 90;
+int gripperAngle = 10; 
+
+bool lastButtonAState = HIGH;
+bool lastButtonBState = HIGH;
+
+void setup() {
+  pinMode(buttonA, INPUT_PULLUP);
+  pinMode(buttonB, INPUT_PULLUP);
+  pinMode(buttonE, INPUT_PULLUP); 
+
+  Braccio.begin();
+  Braccio.ServoMovement(20, baseAngle, shoulderAngle, elbowAngle, 90, 180, gripperAngle);
+}
+
+void loop() {
+  int xVal = analogRead(joyX);
+  int yVal = analogRead(joyY);
+
+  if (xVal < 512 - threshold) {
+    baseAngle -= 5;
+  } else if (xVal > 512 + threshold) {
+    baseAngle += 5;
+  }
+
+  if (yVal < 512 - threshold) {
+    shoulderAngle -= 5;
+  } else if (yVal > 512 + threshold) {
+    shoulderAngle += 5;
+  }
+
+  bool currentButtonAState = digitalRead(buttonA);
+  if (lastButtonAState == HIGH && currentButtonAState == LOW) {
+    elbowAngle += 5;
+    elbowAngle = constrain(elbowAngle, 0, 180);
+  }
+  lastButtonAState = currentButtonAState;
+
+  bool currentButtonBState = digitalRead(buttonB);
+  if (lastButtonBState == HIGH && currentButtonBState == LOW) {
+    elbowAngle -= 5;
+    elbowAngle = constrain(elbowAngle, 0, 180);
+  }
+  lastButtonBState = currentButtonBState;
+
+  
+  if (digitalRead(buttonE) == LOW) {
+    gripperAngle = 70; 
+  } else {
+    gripperAngle = 10; 
+  }
+
+  baseAngle = constrain(baseAngle, 0, 180);
+  shoulderAngle = constrain(shoulderAngle, 15, 165);
+
+  Braccio.ServoMovement(10, baseAngle, shoulderAngle, elbowAngle, 90, 180, gripperAngle);
+}
+
