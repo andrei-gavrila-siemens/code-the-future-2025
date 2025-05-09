465e4d37b7eab9349d27e71c2affa4504837e96e
Valceanu <marioxnoa76@gmail.com>
2025-05-10 01:25:51 +0300

    feat: added automated motion

--- BTD_code/Robot/Robot.ino
+++ BTD_code/Robot/Robot.ino
@@ -20,14 +20,26 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
-int baseAngle_idle = 0;
+int baseAngle_idle = 90;
 int shoulderAngle_idle = 0;
 int elbowAngle_idle = 180;
 int wristVerAngle_idle = 90;
 int wristRotAngle_idle = 90;
 int gripperAngle_idle = 73;
 
-int baseAngle = 0;
+int baseAngle_auto_start = 90;
+int shoulderAngle_auto_start = 0;
+int elbowAngle_auto_start = 180;
+int wristVerAngle_auto_start = 90;
+int wristRotAngle_auto_start = 90;
+
+int baseAngle_auto_end = 90;
+int shoulderAngle_auto_end = 0;
+int elbowAngle_auto_end = 180;
+int wristVerAngle_auto_end = 90;
+int wristRotAngle_auto_end = 90;
+
+int baseAngle = 90;
 int shoulderAngle = 0;
 int elbowAngle = 180;
 int wristVerAngle = 90;
@@ -74,7 +86,23 @@ void setup()
 void loop()
 {
   joystickConfig();
-  // Serial.println(state);
+// Serial.print("xAxisValue: ");
+// Serial.print(xAxisValue);
+// Serial.print(" | yAxisValue: ");
+// Serial.print(yAxisValue);
+// Serial.print(" | aBtnValue: ");
+// Serial.print(aBtnValue);
+// Serial.print(" | bBtnValue: ");
+// Serial.print(bBtnValue);
+// Serial.print(" | cBtnValue: ");
+// Serial.print(cBtnValue);
+// Serial.print(" | dBtnValue: ");
+// Serial.print(dBtnValue);
+// Serial.print(" | eBtnValue: ");
+// Serial.print(eBtnValue);
+// Serial.print(" | fBtnValue: ");
+// Serial.println(fBtnValue);
+
 
   switch (state)
   {
@@ -85,7 +113,6 @@ void loop()
       break;
     case Auto:
       runAuto();
-      Braccio.ServoMovement(60, baseAngle, shoulderAngle, elbowAngle, wristVerAngle, wristRotAngle, gripperAngle);
       break;
     case Manual:
       runManual();
@@ -98,7 +125,7 @@ void runIdle()
 {
   // Serial.println("IDLE");
 
-  baseAngle = 0;
+  baseAngle = 90;
   shoulderAngle = 0;
   elbowAngle = 180;
   wristVerAngle = 180;
@@ -116,15 +143,78 @@ void runManual()
   // Serial.println("RUN");
   armController();
 
+  if (eBtnValue)
+  {
+    Serial.println("SAVED START");
+    baseAngle_auto_start = baseAngle;
+    shoulderAngle_auto_start = shoulderAngle;
+    elbowAngle_auto_start = elbowAngle;
+    wristVerAngle_auto_start = wristVerAngle;
+    wristRotAngle_auto_start = wristRotAngle;
+  }
+
+  if (fBtnValue)
+  {
+    Serial.println("SAVED END");
+    baseAngle_auto_end = baseAngle;
+    shoulderAngle_auto_end = shoulderAngle;
+    elbowAngle_auto_end = elbowAngle;
+    wristVerAngle_auto_end = wristVerAngle;
+    wristRotAngle_auto_end = wristRotAngle;
+  }
+
   if (aBtnValue)
   {
     state = Idle;
+  }
+
+  if (cBtnValue)
+  {
+    state = Auto;
+    cBtnValue = 0;
   }
 }
 
 void runAuto()
 {
-  if (cBtnValue)
+  baseAngle = baseAngle_auto_start;
+  shoulderAngle = shoulderAngle_auto_start;
+  elbowAngle = elbowAngle_auto_start;
+  wristVerAngle = wristVerAngle_auto_start;
+  wristRotAngle = wristRotAngle_auto_start;
+  Braccio.ServoMovement(60, baseAngle, shoulderAngle, elbowAngle - 20, wristVerAngle, wristRotAngle, 10);
+
+  joystickConfig();
+  if (isAnyBtnPressed() && !cBtnValue)
+  {
+    state = Manual;
+  }
+
+  Braccio.ServoMovement(20, baseAngle, shoulderAngle, elbowAngle, wristVerAngle, wristRotAngle, 73);
+  
+  joystickConfig();
+  if (isAnyBtnPressed() && !cBtnValue)
+  {
+    state = Manual;
+  }
+
+  baseAngle = baseAngle_auto_end;
+  shoulderAngle = shoulderAngle_auto_end;
+  elbowAngle = elbowAngle_auto_end;
+  wristVerAngle = wristVerAngle_auto_end;
+  wristRotAngle = wristRotAngle_auto_end;
+  Braccio.ServoMovement(60, baseAngle, shoulderAngle, elbowAngle, wristVerAngle, wristRotAngle, 73);
+
+  joystickConfig();
+  if (isAnyBtnPressed() && !cBtnValue)
+  {
+    state = Manual;
+  }
+
+  Braccio.ServoMovement(60, baseAngle, shoulderAngle, elbowAngle, wristVerAngle, wristRotAngle, 10);
+
+  joystickConfig();
+  if (isAnyBtnPressed() && !cBtnValue)
   {
     state = Manual;
   }
@@ -146,11 +236,13 @@ void armController()
   if (dBtnValue) 
   {
     gripperAngle = 10;
+    // open
   }
 
   if (bBtnValue) 
   {
     gripperAngle = 73;
+    // closed
   }
 }
 
